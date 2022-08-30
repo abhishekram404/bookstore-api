ec540f4238850c10bc66b23ad339fbe1ce52711d
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-08-31 01:02:20 +0545

    Add checkAuth middleware

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,27 @@
+const errorHandler = require("../utils/errorHandler");
+const jwt = require("jsonwebtoken");
+exports.checkAuth = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.status(401).send({
+        success: false,
+        message: "Unauthorized request",
+        data: null,
+      });
+    }
+    const token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).send({
+        success: false,
+        message: "Unauthorized request",
+        data: null,
+      });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    errorHandler({ error, res });
+  }
+};
