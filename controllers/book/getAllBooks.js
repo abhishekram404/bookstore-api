c26fab6539440c73f979d5ac04474090ff3044b9
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-01 00:51:04 +0545

    Create getAllBooks route

---
+++ controllers/book/getAllBooks.js
@@ -0,0 +1,21 @@
+const Book = require("../../models/Book");
+const User = require("../../models/User");
+const errorHandler = require("../../utils/errorHandler");
+
+exports.getAllBooks = async (req, res) => {
+  try {
+    const { user } = await req;
+    const books = await Book.find({
+      $or: [{ owner: user._id }, { heldBy: user._id }],
+    });
+    return res.status(200).send({
+      success: true,
+      message: "All books fetched successfully",
+      data: {
+        books,
+      },
+    });
+  } catch (error) {
+    errorHandler({ error, res });
+  }
+};
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,7 +1,12 @@
+const { checkAuth } = require("../middlewares/checkAuth");
+const { getAllBooks } = require("../controllers/book/getAllBooks");
+
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
   res.send("Book routes");
 });
+
+router.get("/all", checkAuth, getAllBooks);
 
 module.exports = router;
