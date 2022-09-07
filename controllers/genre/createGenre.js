1380187a15a9a42a2e9a1c7e6c9b4020a42e0f92
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-07 22:23:06 +0545

    Add genre already exists check

--- controllers/genre/createGenre.js
+++ controllers/genre/createGenre.js
@@ -4,6 +4,19 @@ const errorHandler = require("../../utils/errorHandler");
 exports.createGenre = async (req, res) => {
   try {
     const { name } = req.body;
+    const slug = name.split(" ").join("-");
+    const alreadyExists = await Genre.exists({
+      slug,
+    });
+
+    if (alreadyExists) {
+      return res.status(400).send({
+        success: false,
+        message: `Genre with name ${name} already exists.`,
+        data: null,
+      });
+    }
+
     const genre = await Genre.create({ name });
     return res.status(200).send({
       success: true,
