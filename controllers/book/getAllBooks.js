e020d6f24008cd89de2c322d0be0e4fc7d50c1e0
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-21 08:22:16 +0545

    Sort books by newest first

--- controllers/book/getAllBooks.js
+++ controllers/book/getAllBooks.js
@@ -1,10 +1,9 @@
 const Book = require("../../models/Book");
-const User = require("../../models/User");
 const errorHandler = require("../../utils/errorHandler");
 
 exports.getAllBooks = async (req, res) => {
   try {
-    const books = await Book.find();
+    const books = await Book.find().sort({ createdAt: -1 });
     return res.status(200).send({
       success: true,
       message: "All books fetched successfully",
