2324490e8a52c397089ffac634619743b45d4613
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-23 23:03:41 +0545

    Create route to get exchanged books

---
+++ controllers/book/getExchangedBooks.js
@@ -0,0 +1,24 @@
+const Book = require("../../models/Book");
+const User = require("../../models/User");
+const errorHandler = require("../../utils/errorHandler");
+
+exports.getExchangedBooks = async (req, res) => {
+  try {
+    const { user } = await req;
+    const books = await Book.find({
+       heldBy: user._id 
+    });
+    return res.status(200).send({
+      success: true,
+      message: "All books fetched successfully",
+      data: {
+        books,
+      },
+    });
+  } catch (error) {
+    errorHandler({ error, res });
+  }
+};
+
+
+ 
