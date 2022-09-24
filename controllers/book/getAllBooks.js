a242c73a531f09175fc7dabf8ae60a1faafd83fd
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-09-24 21:36:17 +0545

    Filter held and exchanged books on homepage

--- controllers/book/getAllBooks.js
+++ controllers/book/getAllBooks.js
@@ -3,7 +3,10 @@ const errorHandler = require("../../utils/errorHandler");
 
 exports.getAllBooks = async (req, res) => {
   try {
-    const books = await Book.find().sort({ createdAt: -1 });
+    const books = await Book.find({
+      heldBy: null,
+      owner: { $ne: req?.user?._id },
+    }).sort({ createdAt: -1 });
     return res.status(200).send({
       success: true,
       message: "All books fetched successfully",
