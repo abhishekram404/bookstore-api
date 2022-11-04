da5af3a2ea2884f5d43579ec1186ef93bea7b91f
Keshav Kishor Ram <ramabishek40@gmail.com>
2022-11-04 20:13:28 +0545

    send ratings in homepage posts

--- controllers/book/getAllBooks.js
+++ controllers/book/getAllBooks.js
@@ -1,20 +1,47 @@
 const Book = require("../../models/Book");
 const errorHandler = require("../../utils/errorHandler");
+const { Types } = require("mongoose");
 
 exports.getAllBooks = async (req, res) => {
   try {
-    const books = await Book.find({
-      heldBy: null,
-      owner: { $ne: req?.user?._id },
-    }).sort({ createdAt: -1 });
+    let booksAggregation = await Book.aggregate([
+      {
+        $match: {
+          // not equal
+          heldBy: null,
+          owner: { $ne: new Types.ObjectId(req?.user?._id) },
+        },
+      },
+      {
+        $lookup: {
+          from: "ratings",
+          localField: "_id",
+          foreignField: "bookId",
+          as: "bookRatings",
+        },
+      },
+      {
+        $addFields: {
+          rating: { $avg: "$bookRatings.rating" },
+        },
+      },
+      {
+        $unset: ["bookRatings"],
+      },
+      {
+        $sort: { createdAt: -1 },
+      },
+    ]);
+
     return res.status(200).send({
       success: true,
       message: "All books fetched successfully",
       data: {
-        books,
+        books: booksAggregation,
       },
     });
   } catch (error) {
+    console.log(error);
     errorHandler({ error, res });
   }
 };
--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -10,7 +10,7 @@ exports.checkAuth = async (req, res, next) => {
       });
     }
     const token = req.headers.authorization.split(" ")[1];
-
+    console.log(token);
     if (!token) {
       return res.status(401).send({
         success: false,
@@ -19,6 +19,8 @@ exports.checkAuth = async (req, res, next) => {
       });
     }
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decoded);
+
     req.user = decoded;
     next();
   } catch (error) {
